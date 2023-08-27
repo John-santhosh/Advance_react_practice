f3463a32f0a644b4b1adbfb06c6e0700b51d1079
John-santhosh <johnsantosh2@gmail.com>
2023-08-27 18:46:05 +0530

    general modifiction

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// import AuthWithJWT from "./Auth with JWT, Referseh tokens & cookies";
+// import Auth2 from "./2.login_&_auth_with_axios";
+import ProtectedRoutes from "./3.protected_routes";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <AuthWithJWT /> */}
+      {/* <Auth2 /> */}
+      <ProtectedRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
